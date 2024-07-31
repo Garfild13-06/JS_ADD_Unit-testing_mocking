f03cdf276108262e7cec359c38c219d911263aa2
garfild <garfild13-06@yandex.com>
2024-07-31 17:45:54 +0300

    update getLevel function

--- src/js/userLevel.js
+++ src/js/userLevel.js
@@ -4,13 +4,11 @@ import {fetchData} from './http';
 export function getLevel(userId) {
     try {
         const response = fetchData(`https://server/user/${userId}`);
-
         if (response.status === 'ok') {
             return `Ваш текущий уровень: ${response.level}`;
         }
     } catch (error) {
         console.error(error);
     }
-
     return 'Информация об уровне временно недоступна';
 }
